e7b5d4b23b27e483c16b5024f9cd1766c643b925
Anix <aniruddha.patwardhan@gmail.com>
2015-01-17 15:37:11 +0530

    db schema for users

--- core/db/users.py
+++ core/db/users.py
@@ -1,6 +1,13 @@
 from mongoengine import *
 import datetime
 
+class address(EmbeddedDocument):
+    address = StringField(max_length = 1000, required = True)
+    city = StringField(max_length = 50, required = True)
+    state = StringField(max_length = 50, required = True)
+    pin = StringField(max_length = 6, required = True)
+    country = StringField(max_length = 50, required = True, default = "India")
+
 
 class user(Document):
     first_name = StringField(max_length = 50, required = True)
@@ -8,14 +15,9 @@ class user(Document):
     email = EmailField(max_length = 100, required = True, default = "girlzfashionz@gmail.com")
     phone1 = StringField(max_length = 10, required = True)
     phone2 = StringField(max_length = 10)
-    address = StringField(max_length = 1000, required = True)
-    city = StringField(max_length = 50, required = True)
-    state = StringField(max_length = 50, required = True)
-    pin = StringField(max_length = 6, required = True)
-    country = StringField(max_length = 50, required = True, default = "India")
+
+    addresses = ListField(EmbeddedDocumentField(address), required = True)
+
     dob = DateTimeField(default = datetime.datetime.now())
     anniversary = DateTimeField(default = datetime.datetime.now())
     gender = StringField(max_length = 6, required = True, default = "female")
-
-
-
--- core/main.py
+++ core/main.py
@@ -2,9 +2,11 @@ from users import *
 
 connect('sellersupport')
 
-anix = user(first_name = "Anix", last_name = "pats", phone1="9902465002", address="DSR elegance", city="bangalore", state="karnataka", pin="560034", gender="male").save()
+anix_address = address(address="DSR elegance", city="bangalore", state="karnataka", pin="560034", gender="male")
+anix = user(first_name = "Anix", last_name = "pats", phone1="9902465002", addresses=[anix_address], gender="male")
+anix.save()
 
 print "doc saved"
 for usr in user.objects():
-    print usr.first_name
+    print usr.first_name, usr.addresses
     usr.delete()
